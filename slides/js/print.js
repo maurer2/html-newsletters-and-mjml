663208c614fcfeec97d9013486594c105f3fe5af
Martin <mr.mmaurer+work@gmail.com>
2020-01-10 22:32:55 +0000

    Added presentation and pdf-export

---
+++ slides/js/print.js
@@ -0,0 +1,32 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('http://localhost:8000/?print-pdf', { waitUntil: 'networkidle2' });
+  await page.emulateMedia('screen');
+  await page.addStyleTag({
+    content: `
+      .reveal pre { box-shadow: none !important; }
+      .reveal .progress { display: none !important; }
+      .reveal .controls { display: none !important; }
+    `,
+  });
+  await page.pdf({
+    path: 'presentation.pdf',
+    format: 'A4',
+    landscape: true,
+    printBackground: true,
+    margin: {
+      top: 0,
+      right: 0,
+      bottom: 0,
+      left: 0,
+    },
+    preferCSSPageSize: true,
+    // width: '297mm',
+    // height: '210mm',
+    displayHeaderFooter: true,
+  });
+  await browser.close();
+})();
